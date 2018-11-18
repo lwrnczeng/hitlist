9ef4c8607cab5417c5547225fe5b6b973b0cc999
Lawrence Zeng <lzeng@lallemand.com>
2018-11-18 15:33:49 -0500

    t

--- HitList/CoreDataStack.swift
+++ HitList/CoreDataStack.swift
@@ -42,7 +42,7 @@ class CoreDataStack: NSObject {
             } catch {
                 fatalError("Error migrating store: \(error)")
             }
-//        }  
+//        }   
     }
     
     
